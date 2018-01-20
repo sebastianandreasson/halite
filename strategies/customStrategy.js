6e59304d62c05b5b924a145fa1fde4d0d2c1306e
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2018-01-20 15:50:30 +0100

    added strategy to check enemies on spawn

--- strategies/customStrategy.js
+++ strategies/customStrategy.js
@@ -4,6 +4,49 @@ const Geometry = require('../hlt/Geometry')
 const chosenPlanets = {}
 const comittedShips = {}
 
+const attackStrategy = (gameMap, ship) => {
+  const enemyShips = gameMap
+    .enemyShips
+    .filter(s => s.isDocked())
+    .sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+
+  if (!enemyShips.length) return null
+
+  const enemyShip = enemyShips[0]
+  return ship.navigate({
+    target: enemyShip,
+    keepDistanceToTarget: 2,
+    speed: constants.MAX_SPEED,
+    avoidObstacles: true,
+    ignoreShips: false
+  })
+}
+
+const checkNearbyEnemies = (gameMap, ship) => {
+  const enemyShips = gameMap
+    .enemyShips
+    .filter(s => Geometry.distance(ship, s) <= 10)
+    .sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+
+  if (!enemyShips.length) {
+    return null
+  }
+
+  const enemyShip = enemyShips[0]
+  return ship.navigate({
+    target: enemyShip,
+    keepDistanceToTarget: 2,
+    speed: constants.MAX_SPEED,
+    avoidObstacles: true,
+    ignoreShips: false
+  })
+}
+
+// TODO docked ships should fight back
+// TODO reach planet and stop, try to fly around it
+// TODO If I have more planets start suiciding ships
+// TODO 4 player strat
+
 /**
  * strategy is a function that accepts the current game map and return a list of next steps to take
  * @param {GameMap} gameMap
@@ -21,41 +64,34 @@ module.exports = (gameMap) => {
 
         // ok so if we can't dock and we're next to the planet lets switch up our plan
         // lets try attacking close ships
-        if (plan.planet && !plan.planet.hasDockingSpot()) {
+        if (plan.attack || plan.planet && !plan.planet.hasDockingSpot()) {
           // seek out closest docked enemyShips
-          const enemyShips = gameMap
-            .enemyShips
-            .filter(s => s.isDocked())
-            .sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
-
-          if (!enemyShips.length) return null
-
-          const enemyShip = enemyShips[0]
-          return ship.navigate({
-            target: enemyShip,
-            keepDistanceToTarget: 2,
-            speed: constants.MAX_SPEED,
-            avoidObstacles: true,
-            ignoreShips: false
-          })
+          return attackStrategy(gameMap, ship)
         }
 
         const speed = Geometry.distance(ship, plan.planet) > 10 ? constants.MAX_SPEED : constants.MAX_SPEED / 2
+        const angularStep = Geometry.distance(ship, plan.planet) > 25 ? 5 : 1
         return ship.navigate({
           target: plan.planet,
           keepDistanceToTarget: plan.planet.radius + 3,
           speed,
+          angularStep,
           avoidObstacles: true,
           ignoreShips: false
         })
       }
+      // if we don't have a plan we're either at the start or ship has just been created
+
+      // before trying to find a free mining spot we should check if an enemy is really close and go attack
+      const attackStrat = checkNearbyEnemies(gameMap, ship)
+      if (attackStrat) return attackStrat
 
       // find the planets that are free or occupied by you
       let planetsOfInterest = gameMap
         .planets
         .filter(p => {
           const chosenPlanet = chosenPlanets[p._params.id]
-          if (chosenPlanet && chosenPlanet.ships > p._params.dockingSpots) {
+          if (chosenPlanet && chosenPlanet.ships >= p._params.dockingSpots) {
             return false
           }
           return true
@@ -69,10 +105,12 @@ module.exports = (gameMap) => {
       }
 
       // sorting planets based on the distance to the ship
-      const sortedPlants = [
-        ...planetsOfInterest
-      ].sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+      const sortedPlants = planetsOfInterest.sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
       const chosenPlanet = sortedPlants[0]
+
+      if (!chosenPlanet) {
+        return attackStrategy(gameMap, ship)
+      }
 
       if (chosenPlanets[chosenPlanet._params.id]) {
         chosenPlanets[chosenPlanet._params.id].ships++
@@ -83,26 +121,17 @@ module.exports = (gameMap) => {
         }
       }
 
-      comittedShips[ship._params.id] = {
-        planet: chosenPlanet
-      }
+      comittedShips[ship._params.id] = { planet: chosenPlanet }
 
-      if (ship.canDock(chosenPlanet)) {
-        return ship.dock(chosenPlanet)
-      }
-      /*
-       If we can't dock, we approach the planet with constant speed.
-       Don't worry about pathfinding for now, as the command will do it for you.
-       We run this navigate command each turn until we arrive to get the latest move.
-       Here we move at half our maximum speed to better control the ships.
-       Navigate command is an example and most likely you will have to design your own.
-       */
+      if (ship.canDock(chosenPlanet)) return ship.dock(chosenPlanet)
+
       return ship.navigate({
         target: chosenPlanet,
         keepDistanceToTarget: chosenPlanet.radius + 3,
         speed: constants.MAX_SPEED,
         avoidObstacles: true,
-        ignoreShips: false
+        ignoreShips: false,
+        angularStep: 1
       })
     })
 
--- strategies/customStrategy_tmp.js
+++ strategies/customStrategy_tmp.js
@@ -1,39 +1,102 @@
 const constants = require('../hlt/Constants')
 const Geometry = require('../hlt/Geometry')
 
-const customSort = (a, b, field) => {
-  if (a[field]() === !b[field]()) return 1
-  if (!a[field]() === b[field]()) return -1
-  return 0
+const chosenPlanets = {}
+const comittedShips = {}
+
+const attackStrategy = (gameMap, ship) => {
+  const enemyShips = gameMap
+    .enemyShips
+    .filter(s => s.isDocked())
+    .sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+
+  if (!enemyShips.length) return null
+
+  const enemyShip = enemyShips[0]
+  return ship.navigate({
+    target: enemyShip,
+    keepDistanceToTarget: 2,
+    speed: constants.MAX_SPEED,
+    avoidObstacles: true,
+    ignoreShips: false
+  })
 }
+
+const checkNearbyEnemies = (gameMap, ship) => {
+  const enemyShips = gameMap
+    .enemyShips
+    .filter(s => Geometry.distance(ship, s) <= 10)
+    .sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+
+  if (!enemyShips.length) {
+    return null
+  }
+
+  const enemyShip = enemyShips[0]
+  return ship.navigate({
+    target: enemyShip,
+    keepDistanceToTarget: 2,
+    speed: constants.MAX_SPEED,
+    avoidObstacles: true,
+    ignoreShips: false
+  })
+}
+
+// TODO docked ships should fight back
+// TODO reach planet and stop, try to fly around it
+// TODO If I have more planets start suiciding ships
+// TODO 4 player strat
 
 /**
  * strategy is a function that accepts the current game map and return a list of next steps to take
  * @param {GameMap} gameMap
  * @returns {string[]} moves that needs to be taken. null values are ignored
  */
-module.exports = function (gameMap) {
-  // Here we build the set of commands to be sent to the Halite engine at the end of the turn
-  // one ship - one command
-  // in this particular strategy we only give new commands to ships that are not docked
+module.exports = (gameMap) => {
+
   const moves = gameMap.myShips
     .filter(s => s.isUndocked())
     .map(ship => {
-      // find the planets that are free or occupied by you
-      if (ship.committed && !ship.isDocked()) {
+      const plan = comittedShips[ship._params.id]
+      if (plan) {
+        // great lets dock if we can
+        if (plan.planet && ship.canDock(plan.planet)) return ship.dock(plan.planet)
+
+        // ok so if we can't dock and we're next to the planet lets switch up our plan
+        // lets try attacking close ships
+        if (plan.attack || plan.planet && !plan.planet.hasDockingSpot()) {
+          // seek out closest docked enemyShips
+          return attackStrategy(gameMap, ship)
+        }
+
+        const speed = Geometry.distance(ship, plan.planet) > 10 ? constants.MAX_SPEED : constants.MAX_SPEED / 2
+        const angularStep = Geometry.distance(ship, plan.planet) > 25 ? 5 : 1
         return ship.navigate({
-          target: ship.committed,
-          keepDistanceToTarget: ship.committed.radius + 3,
-          speed: constants.MAX_SPEED / 2,
+          target: plan.planet,
+          keepDistanceToTarget: plan.planet.radius + 3,
+          speed,
+          angularStep,
           avoidObstacles: true,
           ignoreShips: false
         })
       }
+      // if we don't have a plan we're either at the start or ship has just been created
 
+      // before trying to find a free mining spot we should check if an enemy is really close and go attack
+      const attackStrat = checkNearbyEnemies(gameMap, ship)
+      if (attackStrat) return attackStrat
+
+      // find the planets that are free or occupied by you
       let planetsOfInterest = gameMap
         .planets
+        .filter(p => {
+          const chosenPlanet = chosenPlanets[p._params.id]
+          if (chosenPlanet && chosenPlanet.ships >= p._params.dockingSpots) {
+            return false
+          }
+          return true
+        })
         .filter(p => p.isFree() || (p.isOwnedByMe() && p.hasDockingSpot() ))
-
 
       if (planetsOfInterest.length === 0) {
         planetsOfInterest = gameMap
@@ -42,26 +105,33 @@ module.exports = function (gameMap) {
       }
 
       // sorting planets based on the distance to the ship
-      const sortedPlants = [...planetsOfInterest].sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
+      const sortedPlants = planetsOfInterest.sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))
       const chosenPlanet = sortedPlants[0]
-      ship.committed = chosenPlanet
 
-      if (ship.canDock(chosenPlanet)) {
-        return ship.dock(chosenPlanet)
+      if (!chosenPlanet) {
+        return attackStrategy(gameMap, ship)
       }
-      /*
-       If we can't dock, we approach the planet with constant speed.
-       Don't worry about pathfinding for now, as the command will do it for you.
-       We run this navigate command each turn until we arrive to get the latest move.
-       Here we move at half our maximum speed to better control the ships.
-       Navigate command is an example and most likely you will have to design your own.
-       */
+
+      if (chosenPlanets[chosenPlanet._params.id]) {
+        chosenPlanets[chosenPlanet._params.id].ships++
+      } else {
+        chosenPlanets[chosenPlanet._params.id] = {
+          planet: chosenPlanet,
+          ships: 1
+        }
+      }
+
+      comittedShips[ship._params.id] = { planet: chosenPlanet }
+
+      if (ship.canDock(chosenPlanet)) return ship.dock(chosenPlanet)
+
       return ship.navigate({
         target: chosenPlanet,
         keepDistanceToTarget: chosenPlanet.radius + 3,
-        speed: constants.MAX_SPEED / 2,
+        speed: constants.MAX_SPEED,
         avoidObstacles: true,
-        ignoreShips: false
+        ignoreShips: false,
+        angularStep: 1
       })
     })
 
